b9904584505547d6d00342df59d94f3c76ca98d6
Anmol <39215676+ScripterAnmol@users.noreply.github.com>
2023-05-02 10:03:24 +0530

    Create main.cpp

---
+++ C. Ball in Berland Round 697 Codeforces/main.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+// #define ll int
+#define dd double
+#define vl vector<ll>
+#define vvl vector<vl>
+#define vd vector<dd>
+#define vvd vector<vd>
+#define loop ll t; cin>>t; while(t--)
+#define all(a) a.begin(), a.end()
+#define sz 100005
+#define MOD 1000000007
+#define debug cout << "here" << endl;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define pb push_back
+#define ff first
+#define ss second
+#define pl pair<ll, ll>
+
+ll add(ll a,ll b,ll mod){
+    ll result =  (a+b)%mod;
+    if(result<0){
+        result+=mod;
+    }
+    return result;
+}
+
+ll mul(ll a,ll b,ll mod){
+    ll result =  (1LL*a*b)%mod;
+    if(result<0){
+        result+=mod;
+    }
+    return result;
+}
+
+ll power(ll a, ll b, ll mod) {
+    ll res = 1;
+    while(b){
+        if((b%2)==1){
+            res=mul(res,a,mod);
+        }
+        a=mul(a,a,mod);
+        b/=2;
+    }
+    return res;
+}
+ 
+ll inverse(ll x, ll p) {
+    return power(x, p - 2, p);
+}
+
+void solve(){
+    ll n,m,k;
+    cin>>n>>m>>k;
+    vl arr(k);
+    for(ll i=0;i<k;i++){
+        cin>>arr[i];
+    }
+    
+    vl deg(n+m+1,0);
+    vector<pair<ll,ll>> adj;
+    ll temp;
+    for(ll i=0;i<k;i++){
+        cin>>temp;
+        deg[arr[i]]++;
+        deg[n+temp]++;
+        adj.pb({arr[i],n+temp});
+    }
+    
+    ll count=0;
+    for(auto i=0;i<k;i++){
+        count+=(k-(deg[adj[i].first]+deg[adj[i].second]-1));
+    }
+    
+    cout<<count/2<<"\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    loop
+    solve();
+
+    return 0;
+}
